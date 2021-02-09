dc5589dbcf329e70fbde990860ccfca5e8dc6884
loriortiz <lori@loriortiz.com>
2021-02-09 16:24:18 -0500

    Remove required from form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,15 +5,15 @@ const ContactForm = (props) => (
   <form
     name="contact" 
     method="post" 
-    data-netlify="true"  
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
     >
-    <input type="hidden" name="bot-field" />
     <input type="hidden" name="form-name" value="contact" />
     <div class="user-info">
         <label htmlFor="name">Name *</label>
-        <input type="text" name="name" id="name" aria-required/>
+        <input type="text" name="name" id="name" />
         <label htmlFor="email">Email *</label>
-        <input type="text" name="email" id="email" aria-required/>
+        <input type="text" name="email" id="email" />
     </div>
     <div class="message-box">
         <label htmlFor="message">Your issues or interests *</label>
